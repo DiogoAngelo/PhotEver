9cbc34ea06902c1f1bc5599d5204d547cc8dec6f
Diogo Santos <diogo.angelo@designa.com.br>
2023-01-02 19:43:52 +0000

    Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
@@ -15,12 +16,11 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'sign-in',
-        pathMatch: 'full',
+        component: SignInComponent,
       },
       {
-        path: 'sign-in',
-        component: SignInComponent,
+        path: 'sign-up',
+        component: SignUpComponent,
       },
     ],
   },

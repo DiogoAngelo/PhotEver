97fda8059db2e48d51193840a2659dbc5bb6d89d
Diogo Santos <diogo.angelo@designa.com.br>
2023-01-12 15:59:42 +0000

    Listing photo in grid [TEMP]

--- src/app/pages/photos/photo-list/photos/photos.component.ts
+++ src/app/pages/photos/photo-list/photos/photos.component.ts
@@ -1,10 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhotoModel } from 'src/app/shared/models/photo.model';
 
 @Component({
   selector: 'base-photos',
   templateUrl: './photos.component.html',
 })
-export class PhotosComponent {
+export class PhotosComponent implements OnChanges {
   @Input() public photos!: PhotoModel[];
+  public rows: any[] = [];
+
+  public ngOnChanges(change: SimpleChanges): void {
+    if (change.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
+  }
+
+  public groupColumns(photos: PhotoModel[]): any {
+    const newRows = [];
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
+  }
 }

c56d1ee62f1f69873ae5a0f22d38931958f9cc6a
Diogo Santos <diogo.angelo@designa.com.br>
2023-01-13 18:21:19 +0000

    Debounce to filter

--- src/app/pages/photos/photo-list/photo-list.component.ts
+++ src/app/pages/photos/photo-list/photo-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { userService } from 'src/app/services/user.service';
 import { PhotoModel } from 'src/app/shared/models/photo.model';
 
 @Component({
   templateUrl: './photo-list.component.html',
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   constructor(
     private userService: userService,
     private activatedRoute: ActivatedRoute
@@ -15,12 +17,17 @@ export class PhotoListComponent implements OnInit {
   public photoList: PhotoModel[] = [];
   public userName = this.userService.getUserName();
   public filter: string = '';
+  public debounceFilter: Subject<any> = new Subject<any>();
 
   public ngOnInit(): void {
     this.photoList = this.activatedRoute.snapshot.data.photos;
+
+    this.debounceFilter.pipe(debounceTime(300)).subscribe((filteredText) => {
+      this.filter = filteredText.target.value;
+    });
   }
 
-  public onFilter(event: any) {
-    this.filter = event.target.value;
+  public ngOnDestroy(): void {
+    this.debounceFilter.unsubscribe();
   }
 }
